215fba4309754474fb270f70ee3781817c7cac20
xiawei <xiawei@lymsh.com>
2019-02-28 22:27:20 +0800

    登录页面修改

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -18,16 +18,14 @@
       </div>
     </div>
     <div class="login-main">
-      <div class="login-box">
-        <div class="login-box-bg">
-          <div class="login-box-left">
-            <img :src="login_logo" width="465" height="364" alt="登录logo">
-          </div>
-          <div class="login-box-right">
-            <div class="login-from">
-              <div class="login-from-title"><p>登&nbsp;&nbsp;&nbsp;&nbsp;录</p></div>
+      <div class="login-banner">
+        <div class="login-banner-content">
+          <img :src="BG" class="bg-style" alt="背景图片">
+          <div class="login-from">
+            <div class="login-from-content">
+              <div class="login-from-title">账户登录</div>
               <div class="login-from-body">
-                <el-form ref="form" :model="loginForm" label-width="80px">
+                <el-form ref="form" :model="loginForm">
                   <el-form-item>
                     <el-input v-model="loginForm.username" placeholder="请输入手机号"></el-input>
                   </el-form-item>
@@ -44,12 +42,19 @@
         </div>
       </div>
     </div>
+    <div class="footer">
+      <div class="link"><a >关于我们</a>|<a>联系我们</a></div>
+      <div class="copyright">
+        Copyright&nbsp;©&nbsp;2019&nbsp;&nbsp;金典教育&nbsp;版权所有
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import userSet from '@/assets/icon/set.png'
   import userGif from '@/assets/user.gif'
+  import BG from '@/assets/bg.jpg'
   import login_logo from '@/assets/login/u581.jpg'
   import Cookies from 'js-cookie'// 用于测试设置token后续删除
   export default {
@@ -62,6 +67,7 @@
         login_logo,
         userGif,
         userSet,
+        BG,
         loginForm: {
           username: '', // 用户名
           password: '' // 密码
@@ -136,29 +142,20 @@
   /* reset element-ui css */
   .login-container {
     .el-form-item{
-      margin-bottom: 30px;
-    }
-    .el-form-item__label{
-      color: #fff;
-      font-size: 28px;
-      width: 110px!important;
-      font-weight: normal;
-    }
-    .el-form-item__content{
-      margin-left: 110px!important;
+      margin-bottom: 20px;
     }
     .el-button--medium{
       border-width: 0px;
       width: 100%;
       height: 40px;
-      background: inherit;
-      background-color: rgba(22, 155, 213, 1);
       border: none;
       border-radius: 5px;
       -moz-box-shadow: none;
       -webkit-box-shadow: none;
       box-shadow: none;
       font-size: 20px;
+      background:rgba(81,138,255,1);
+      border-radius:3px;
     }
     .el-button--medium:hover{
       background-color: #66b1ff;
@@ -172,75 +169,6 @@
     }
 
   }
-  @media (max-width: 1199px)  and (min-width: 1024px){
-    .login-container{
-      .el-form-item{
-        margin-bottom: 20px;
-      }
-      .el-input--medium .el-input__inner{
-        height: 35px;
-        line-height: 35px;
-      }
-      .el-button--medium{
-        width: 110px;
-        height: 35px;
-        font-size: 18px;
-        padding: 5px 20px;
-      }
-      .el-button+.el-button {
-        margin-left: 66px;
-      }
-      .el-form-item__label{
-        font-size: 20px;
-      }
-    }
-  }
-  @media (max-width: 1023px)  and (min-width: 769px) {
-    .login-container{
-      .el-form-item{
-        margin-bottom: 15px;
-      }
-      .el-input--medium .el-input__inner{
-        height: 30px;
-        line-height: 30px;
-      }
-      .el-button--medium{
-        width: 90px;
-        height: 30px;
-        font-size: 16px;
-        padding: 5px 20px;
-      }
-      .el-button+.el-button {
-        margin-left: 55px;
-      }
-      .el-form-item__label{
-        font-size: 20px;
-      }
-    }
-  }
-  @media screen and (max-width: 768px) {
-    .login-container{
-      .el-form-item{
-        margin-bottom: 20px;
-      }
-      .el-input--medium .el-input__inner{
-        height: 40px;
-        line-height: 40px;
-      }
-      .el-button--medium{
-        width: 95px;
-        height: 35px;
-        font-size: 20px;
-        padding: 5px 20px;
-      }
-      .el-button+.el-button {
-        margin-left: 45px;
-      }
-      .el-form-item__label{
-        font-size: 20px;
-      }
-    }
-  }
 </style>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
@@ -253,7 +181,7 @@
     height: 100%;
     width: 100%;
     background-color: $bg;
-    padding-top: 64px;
+    padding-top: 80px;
     .login-top{
       position: absolute;
       left: 0px;
@@ -320,10 +248,10 @@
       }
     }
     .login-main{
+      position: relative;
       border-width: 0px;
       position: relative;
       margin: 0 auto;
-      margin-top: 64px;
       background: inherit;
       background-color: rgba(255, 255, 255, 1);
       border: none;
@@ -332,172 +260,83 @@
       -webkit-box-shadow: none;
       box-shadow: none;
       display: table;
-      .login-box{
-        display: table-cell;
-        vertical-align: middle;
-        text-align: center;
-        border-width: 0px;
+      width: 100%;
+      .login-banner{
+        width: 100%;
         position: relative;
-        margin: 0 auto;
+        z-index: 1;
+        background:rgba(196,231,241,1);
+      .login-banner-content{
+        display: block;
         width: 1000px;
-        height: 382px;
-        font-style: normal;
-        font-size: 13px;
-        color: #333333;
-        text-align: center;
-        line-height: normal;
-        .login-box-bg{
-          display: table-cell;
-          vertical-align: middle;
-          text-align: center;
-          border-width: 0px;
-          position: relative;
-          width: 1000px;
-          height: 382px;
-          background: inherit;
-          background-color: rgba(102, 102, 102, 1);
-          border: none;
-          border-radius: 0px;
-          -moz-box-shadow: none;
-          -webkit-box-shadow: none;
-          box-shadow: none;
-          .login-box-left{
-            float: left;
-            width: 480px;
-            height: 100%;
-            background: transparent;
-            img{
-              display: block;
-              margin-top: 8px;
-              margin-left: 5px;
+        height: 350px;
+        margin: 0 auto;
+        position: relative;
+        .bg-style{
+          display: block;
+          width: 100%;
+          height: 100%;
+        }
+        .login-from{
+          position: absolute;
+          right: 20px;
+
+          top: 25px;
+          width: 280px;
+          height: 300px;
+          background:rgba(255,255,255,1);
+          box-shadow:0px 2px 22px 0px rgba(222,219,219,0.5);
+          .login-from-content{
+            padding: 20px 15px;
+            .login-from-title{
+              margin-top: 20px;
+              width:80px;
+              height:28px;
+              font-size:20px;
+              font-family:PingFangSC-Semibold;
+              font-weight:600;
+              color:rgba(51,51,51,1);
+              line-height:28px;
+            }
+            .login-from-body{
+              margin-top: 20px;
             }
           }
-          .login-box-right{
-            float: left;
-            width: 400px;
-            height: 100%;
-            position: relative;
-            .login-from{
-              width: 100%;
-              height: 100%;
-              position: relative;
-              .login-from-title{
-                margin-top: 35px;
-                width:100%;
-                border-width: 0px;
-                height: 41px;
-                font-size: 36px;
-                color: #000000;
-                p{
-                  margin: 0;
-                }
-              }
-              .login-from-body{
-                margin-top: 40px;
-              }
-            }
-          }
+        }
+      }
+      }
+    }
+    .footer{
+      margin-top: 20px;
+      font-size: 12px;
+      color: #666;
+      .link{
+        margin-bottom: 10px;
+        a{
+          margin: 0 10px;
         }
       }
     }
   }
   @media screen and (min-width: 1400px) {
     .login-container .login-main{
-      padding:100px;
     }
   }
   @media (max-width: 1399px)  and (min-width: 1200px) {
     .login-container .login-main{
-      padding:50px;
+
     }
   }
   @media (max-width: 1199px)  and (min-width: 1024px) {
     .login-container .login-main{
-      padding:40px;
-      .login-box{
-        width: 850px;
-        height: 325px;
-        .login-box-bg{
-          width: 850px;
-          height: 325px;
-          .login-box-left{
-            width: 419px;
-            img{
-              display: block;
-              margin-top: 8px;
-              margin-left: 5px;
-              width: 409px;
-              height: 305px;
-            }
-          }
-          .login-box-right{
-            width: 400px;
-            .login-from{
-              .login-from-title{
-                margin-top: 20px;
-                height: 30px;
-                font-size: 28px;
-              }
-            }
-          }
-        }
-      }
     }
   }
   @media (max-width: 1023px)  and (min-width: 769px) {
     .login-container .login-main{
-      padding:30px;
-      .login-box{
-        width: 700px;
-        height: 267px;
-        .login-box-bg{
-          width: 700px;
-          height: 267px;
-          .login-box-left{
-            width: 330px;
-            img{
-              display: block;
-              margin-top: 8px;
-              margin-left: 5px;
-              width: 320px;
-              height: 250px;
-            }
-          }
-          .login-box-right{
-            width: 350px;
-            .login-from{
-              .login-from-title{
-                margin-top: 15px;
-                height: 30px;
-                font-size: 28px;
-              }
-            }
-          }
-        }
-      }
     }
   }
   @media screen and (max-width: 768px) {
     .login-container .login-main{
-      .login-box{
-        width: 370px;
-        .login-box-bg{
-          width: 370px;
-          .login-box-left{
-            display: none;
-          }
-          .login-box-right{
-            width: 350px;
-            .login-from{
-              .login-from-title{
-                margin-top: 15px;
-                height: 30px;
-                font-size: 28px;
-              }
-            }
-          }
-        }
-      }
     }
   }
 </style>
